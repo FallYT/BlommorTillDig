85bc0596d384d3c201dc0cf083ea34ac246c1faf
Fall <62287602+FallYT@users.noreply.github.com>
2024-12-03 21:50:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const plantButton = document.getElementById("plantButton");
   const waterButton = document.getElementById("waterButton");
   const seed = document.getElementById("seed");
+  const stem = document.getElementById("stem");
   const flower = document.getElementById("flower");
+  const card = document.getElementById("card");
 
   plantButton.addEventListener("click", () => {
     seed.classList.remove("hidden");
@@ -12,7 +14,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   waterButton.addEventListener("click", () => {
     seed.classList.add("hidden");
-    flower.classList.remove("hidden");
+    stem.classList.remove("hidden");
+
+    setTimeout(() => {
+      flower.classList.remove("hidden");
+
+      setTimeout(() => {
+        card.classList.remove("hidden");
+      }, 2000);
+    }, 2000);
+
     waterButton.classList.add("hidden");
   });
 });
