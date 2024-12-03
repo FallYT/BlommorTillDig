5ce0ba118e7ffb867ae06d68064a77cc0e2e515c
Fall <62287602+FallYT@users.noreply.github.com>
2024-12-03 22:06:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function waterPlant() {
-  // Trigger flower growth animations
-  document.querySelector('.seed').style.animation = 'plantSeed 2s forwards';
-  document.querySelector('.stem').style.animation = 'growStem 4s forwards';
-  document.querySelector('.leaves').style.animation = 'growLeaves 6s forwards';
-  document.querySelector('.flower').style.animation = 'bloomFlower 6s forwards';
-  document.querySelector('.card').style.animation = 'fallCard 8s forwards';
-}
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
